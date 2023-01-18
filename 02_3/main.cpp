7700f209f72f5f8fb5a3b3fd4b2049848c788883
YutoArake <k021g1420@g.neec.ac.jp>
2023-01-18 15:57:09 +0900

    直し

--- 02_3/main.cpp
+++ 02_3/main.cpp
@@ -4,7 +4,7 @@
 
 // [手順1] 双方向リストの構造体の定義
 typedef struct cell {
-	int val;
+	char station[32];
 	struct cell* prev;
 	struct cell* next;
 } CELL;
@@ -16,13 +16,13 @@ typedef struct cell {
 /// </summary>
 /// <param name="currentCell">リストの現在のセル</param>
 /// <param name="buf">挿入する値</param>
-void Create(CELL* currentCell, int val);
+void Create(CELL* currentCell, const char* buf);
 
 /// <summary>
 /// 一覧を表示する関数
 /// </summary>
 /// <param name="cell">リストの最初のセル</param>
-void Index(CELL* cell);
+void Index(CELL* cell, int cmd);
 
 /// <summary>
 /// 任意のセルのアドレスを返す関数
@@ -32,33 +32,67 @@ void Index(CELL* cell);
 CELL* GetInsertCellAddress(CELL* cell, int itr);
 
 int main() {
-	// データ用変数
+	// 挿入するセル
+	char station[32];
 	int iterator;
-	int inputValue;
 	CELL* insertCell;
 	
-	// [手順2] 先頭のセルの宣言
+	// 先頭のセルの宣言
 	CELL head;
 	head.prev = nullptr;
 	head.next = nullptr;
+	
+	// 駅一覧
+	const int STATION_NUM = 47;
+	const char* stationList[STATION_NUM] = {
+		"大船", "本郷台", "港南台", "洋光台", "新杉田",
+		"磯子", "根岸", "山手", "石川町", "関内",
+		"桜木町", "横浜", "東神奈川", "新子安", "鶴見",
+		"川崎", "蒲田", "大森", "大井町", "品川",
+		"高輪ゲートウェイ", "田町", "浜松町", "新橋", "有楽町",
+		"東京", "神田", "秋葉原", "御徒町", "上野",
+		"鶯谷", "日暮里", "西日暮里", "田端", "上中里",
+		"王子", "東十条", "赤羽", "川口", "西川口",
+		"蕨", "南浦和", "浦和", "北浦和", "与野",
+		"さいたま新都心", "大宮"
+	};
 
-	// コマンド
-	//COMMAND command = NONE;
+	// 高輪ゲートウェイ以外の駅をセルに挿入
+	for (int itr = 0; itr < STATION_NUM; itr++) {
+		if (itr != 20) {
+			insertCell = GetInsertCellAddress(&head, itr);	// 挿入したいセルのアドレスを取得 
+			strcpy_s(station, 32, stationList[itr]);	// 駅名をコピー
+			Create(insertCell, station);				// セルの追加
+		}
+	}
+
+	// 選択肢
+	int command = 0;
 
 	// ループ
 	while (true) {
-		printf("何番目のセルの後ろに挿入してますか？\n");
-		scanf_s("%d", &iterator);
+		printf("[京浜東北線駅一覧]\n");
+		printf("高輪ゲートウェイが追加される前か後か\n");
+		printf("前 : 1、後 : 2\n");
+		scanf_s("%d", &command);
+		printf("\n");
 
-		printf("挿入する値を入力してください？\n");
-		scanf_s("%d", &inputValue);
-
-		// [手順3] 任意のセルの後ろに追加
-		insertCell = GetInsertCellAddress(&head, iterator);	// 挿入したいセルのアドレスを取得 
-		Create(insertCell, inputValue);		// セルの追加
+		// 高輪ゲートウェイを追加
+		if (command == 2) {
+			iterator = 20;
+			// [手順3] 任意のセルの後ろに追加
+			insertCell = GetInsertCellAddress(&head, iterator);	// 挿入したいセルのアドレスを取得 
+			strcpy_s(station, 32, stationList[iterator]);	// 駅名をコピー
+			Create(insertCell, station);							// セルの追加
+		}
+		
+		printf("一覧する順番を決めてください\n");
+		printf("北行 : 1、南行 : 2\n");
+		scanf_s("%d", &command);
 
 		// [手順4] リスト一覧の表示
-		Index(&head);
+		Index(&head, command);
+		printf("\n");
 	}
 
 	return 0;
@@ -66,38 +100,54 @@ int main() {
 
 // 関数定義
 
-void Create(CELL* currentCell, int val) {
+void Create(CELL* currentCell, const char* buf) {
 	CELL* newCell;	// 新規作成するセル
 	// 新規作成する分のメモリを確保する
 	newCell = (CELL*)malloc(sizeof(CELL));
 
 	// 新規作成するセルにデータを代入する
-	newCell->val = val;
+	strcpy_s(newCell->station, 32, buf);
 	newCell->prev = currentCell;
 	newCell->next = currentCell->next;
 
 	// 次のセルの前のアドレスを書き換える
-	CELL* nextCell = currentCell->next;
-	nextCell->prev = newCell;
+	if (currentCell->next != nullptr) {
+		CELL* nextCell = currentCell->next;
+		nextCell->prev = newCell;
+	}
 
 	// 現在のセルに次のセルのアドレスを渡す
 	currentCell->next = newCell;
 }
 
-void Index(CELL* cell) {
-	int num = 1;
+void Index(CELL* cell, int cmd) {
 	// // 最後尾のセルになるまで続く
-	while (cell->next != nullptr) {
-		// 次のセルへ
-		cell = cell->next;
-		printf("%d", num);
-		// セルの中身を表示
-		printf("%p", cell->prev);
-		printf("%5d", cell->val);
-		printf("(%p)", cell);
-		printf("%p\n", cell->next);
-
-		num++;
+	if (cmd == 1) {
+		int num = 1;
+		while (cell->next != nullptr) {
+			// 次のセルへ
+			cell = cell->next;
+			printf("JK ");
+			printf("%2d : ", num);
+			// セルの中身を表示
+			printf("%s", cell->station);
+			printf("\n");
+			num++;
+		}
+	}
+	else if (cmd == 2) {
+		int num = 47;
+		cell = GetInsertCellAddress(cell, num);
+		while (cell->prev != nullptr) {
+			printf("JK ");
+			printf("%2d : ", num);
+			// セルの中身を表示
+			printf("%s", cell->station);
+			printf("\n");
+			// 次のセルへ
+			cell = cell->prev;
+			num--;
+		}
 	}
 }
 
--- AssessmentTask1/main.cpp
+++ AssessmentTask1/main.cpp
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <algorithm>
 
+// 定数
+const int WORDCOUNT = 32;
+
+// 双方向リストの構造体
+typedef struct cell {
+	char str[WORDCOUNT];			// 文字列
+	struct cell* prev;	// 前のセル
+	struct cell* next;	// 次のセル
+} CELL;
 
 // コマンド
 typedef enum COMMAND {
@@ -10,11 +20,36 @@ typedef enum COMMAND {
 	INSERT,		// 挿入
 	EDIT,		// 編集
 	ERASE,		// 削除
+	SORT,		// 並び替え
+	BACK = 9,	// 戻る
 };
 
+// 関数プロトタイプ宣言
+
+// 一覧を表示する関数
+void ShowIndex(CELL* cell);
+// 指定した要素だけ表示する関数
+void ShowIndex(CELL* cell, int itr);
+// 任意のセルのアドレスを返す関数
+CELL* GetInsertCellAddress(CELL* cell, int itr);
+// セルを挿入する
+void InsertCell(CELL* currentCell, const char* buf);
+// セルのリストを編集する
+void EditCell(CELL* cell, int itr, const char* buf);
 
 int main() {
+	// リスト用変数宣言
+	int iterator;	// イテレータ
+	char inputStr[WORDCOUNT];		// 挿入する文字列
+	CELL* insertCell;					// 挿入するセル
 
+	// 先頭のセルの宣言
+	CELL head;
+	head.prev = nullptr;
+	head.next = nullptr;
+
+	// コマンド
+	COMMAND command = NONE;
 
 	while (true) {
 		// コマンド選択
@@ -23,15 +58,222 @@ int main() {
 		printf("2.要素の挿入\n");
 		printf("3.要素の編集\n");
 		// 要素が0個のときは非表示
-		printf("4.要素の削除\n");
-		printf("5.要素の並び替え(オプション)\n");
+		if (head.next != nullptr) {
+			printf("4.要素の削除\n");
+			printf("5.要素の並び替え(オプション)\n");
+		}
 		// 改行
 		printf("\n");
 		printf("------------------------------------------\n");
 		printf("操作を数字で選択してください\n");
+		scanf_s("%d", &command);	// 入力
 
-		scanf_s("%d", )
+		switch (command)
+		{
+		case SHOW:
+			while (true) {
+				// コマンド選択
+				printf("[要素の表示]\n");
+				printf("1.要素の一覧表示\n");
+				printf("2.順番を指定して要素を表示\n");
+				printf("9.要素操作に戻る\n");
+				// 改行
+				printf("\n");
+				printf("操作を数字で選択してください\n");
+				scanf_s("%d", &command);	// 入力
+
+				// 要素の一覧表示
+				if (command == 1) {
+					// 一覧
+					ShowIndex(&head);
+
+					// 次の操作を入力
+					scanf_s("%d", &command);
+
+					// 1ならcontinue、2ならbreakする
+					if (command == 1) {
+						continue;
+					}
+					else if (command == 2) {
+						break;
+					}
+
+				}
+				// 順番を指定して要素を表示
+				else if (command == 2) {
+					// 指定した要素を表示
+					ShowIndex(&head, iterator);
+
+					// 次の操作を入力
+					scanf_s("%d", &command);	// 入力
+
+					// 1ならcontinue、2ならbreakする
+					if (command == 1) {
+						continue;
+					}
+					else if (command == 2) {
+						break;
+					}
+
+				}
+				// 要素操作に戻る
+				else if (command == 9) {
+					break;
+				}
+			}
+			break;
+
+		case INSERT:
+			printf("[リスト要素の追加]\n");
+			printf("要素を追加する場所を指定してください。最後尾に追加する場合は0を入力してください。\n");
+			printf("※現在のセルの総数よりも大きい値の場合も最後尾に追加します。\n");
+			scanf_s("%d", &iterator);
+			printf("\n");
+			printf("追加する要素の値(文字)を入力してください\n");
+			scanf_s("%s", inputStr, WORDCOUNT);
+			printf("\n");
+
+			// イテレータが0だったら
+			if (iterator == 0) iterator = 99;
+
+			// 挿入するセルのアドレスを取得
+			insertCell = GetInsertCellAddress(&head, iterator);
+			// セルを挿入
+			InsertCell(insertCell, inputStr);
+
+			// 確認
+			printf("要素”%s”が%d番目に挿入されました\n", inputStr, iterator);
+			break;
+
+		case EDIT:
+			// 編集
+			EditCell(&head, iterator, inputStr);
+			break;
+
+		case ERASE:
+			printf("[要素の削除]\n");
+			printf("削除したい要素の番号を指定してください。\n");
+			scanf_s("%d", &iterator);
+			printf("\n");
+			printf("番目の要素””を削除しました。\n");
+
+			printf("番目の要素が見つかりませんでした。\n");
+			break;
+		case SORT:
+			break;
+		default:
+			break;
+		}
+
+		// 改行
+		printf("\n");
 	}
 
 	return 0;
+}
+
+// 関数定義
+void ShowIndex(CELL* cell) {
+	int num = 0;
+
+	printf("[要素の一覧表示]\n");
+	printf("要素一覧 : {\n");
+
+	// // 最後尾のセルになるまで続く
+	while (cell->next != nullptr) {
+		printf("%d:”%s”\n", num, cell->str);
+		num++;
+	}
+
+	printf("}\n");
+	printf("\n");
+	// 要素数
+	printf("要素数:%d\n", ++num);
+	printf("\n");
+	printf("------------------------------------------\n");
+	printf("1.要素の表示に戻る\n");
+	printf("2.要素の操作に戻る\n");
+}
+
+void ShowIndex(CELL* cell, int itr) {
+	printf("[順番を指定して要素を表示]\n");
+	printf("表示したい要素の順番を指定してください。最後尾の場合は0を入力してください。\n");
+	printf("※現在のセルの総数よりも大きい値の場合も最後尾にします。\n");
+	scanf_s("%d", &itr);	// 入力
+	printf("\n");
+	// 指定したセルのアドレスを取得
+	CELL* selectCell = GetInsertCellAddress(cell, itr);
+	// 表示
+	printf("%d:%s", itr, selectCell->str);
+
+	printf("\n");
+	printf("------------------------------------------\n");
+	printf("1.要素の表示に戻る\n");
+	printf("2.要素の操作に戻る\n");
+}
+
+CELL* GetInsertCellAddress(CELL* cell, int itr) {
+	for (int i = 0; i < itr; i++) {
+		if (cell->next != nullptr) {
+			cell = cell->next;
+		}
+		else {	// セルの総数よりも多いイテレータの値だったらループを抜ける
+			break;
+		}
+	}
+	return cell;
+}
+
+void InsertCell(CELL* currentCell, const char* buf) {
+	CELL* newCell;	// 新規作成するセル
+	// 新規作成する分のメモリを確保する
+	newCell = (CELL*)malloc(sizeof(CELL));
+
+	// 新規作成するセルにデータを代入する
+	newCell->prev = currentCell;
+	newCell->next = nullptr;
+	// newCellのstrの中身へコピーする(アドレスを渡さないで)
+	strcpy_s(newCell->str, WORDCOUNT, buf);
+
+	// 選択したセルに次のアドレスが入っていれば
+	if (currentCell->next != nullptr) {
+		CELL* nextCell = currentCell->next;
+		nextCell->prev = newCell;
+		newCell->next = nextCell;
+	}
+
+	// 現在のセルに次のセルのアドレスを渡す
+	currentCell->next = newCell;
+}
+
+void EditCell(CELL* cell, int itr, const char* buf) {
+	int num = 0;
+	CELL* cellNum = cell;
+
+	for (int i = 0; i < itr; i++) {
+		if (cellNum->next != nullptr) {
+			cellNum = cellNum->next;
+			num++;
+		}
+		else {	// セルの総数よりも多いイテレータの値だったらループを抜ける
+			break;
+		}
+	}
+
+	printf("[要素の編集]\n");
+	printf("編集したい要素の番号を指定してください。\n");
+	scanf_s("%d", &itr);
+	printf("\n");
+
+	if (itr > 0 && itr <= num) {
+		printf("%d番目の要素の変更する値(文字)を入力してください。\n", itr);
+		scanf_s("%s", buf, WORDCOUNT);
+		printf("\n");
+		// 指定したセルのアドレスを取得
+		CELL* selectCell = GetInsertCellAddress(cell, itr);
+		printf("%d番目の要素の値(文字)が”%s”に変更されました。\n", itr, buf);
+	}
+	else {
+		printf("番目の要素が見つかりませんでした。\n");
+	}
 }
